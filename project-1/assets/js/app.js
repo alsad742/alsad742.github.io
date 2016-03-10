9285a1713769e52c64cc2db6fb86b5b0352ed0dd
daniela <alsad742@newschool.edu>
2016-03-10 09:32:43 -0500

    js exercise

--- project-1/assets/js/app.js
+++ project-1/assets/js/app.js
@@ -77,6 +77,6 @@ $(".overlay").click(function(){
     $(".overlay, .r1img1xx, .r1img2xx, .r1img3xx, .r1img4xx, .r1img5xx, r2img3xx, .r2img4xx, .r2img7xx, .r2img8xx, .r3img1xx, .r3img2xx, .r3img3xx").hide(400);
 });
 
-$(".r1img1x, .r1img2x, .r1img3x, .r1img4x, .r1img5x, r2img3x, .r2img4x, .r2img7x, .r2img8x, .r3img1x, .r3img2x, .r3img3x").click(function(){
+$(".r1img1x, .r1img2x, .r1img3x, .r1img4x, .r1img5x, .r2img3x, .r2img4x, .r2img7x, .r2img8x, .r3img1x, .r3img2x, .r3img3x").click(function(){
     $(".overlay").show(400);
 });
